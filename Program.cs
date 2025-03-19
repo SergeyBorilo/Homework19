0a3f113fdbed889409427bfd505d4114fce2d1fd
SergeyBorilo <S_Barya@i.ua>
2025-03-19 20:08:39 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿var directoryPath = args.Length > 0 ? args[0] : GetDirectoryPath();
+
+if (Directory.Exists(directoryPath))
+{
+    List<string> directories = [];
+    TraverseDirectory(directoryPath, directories);
+    PrintDirectoriesAndFiles(directories);
+}
+else
+    Console.WriteLine("Invalid directory path.");
+
+return;
+
+static string GetDirectoryPath()
+{
+    Console.Write("Enter the directory path: ");
+    return Console.ReadLine();
+}
+
+static void TraverseDirectory(string path, List<string> directories)
+{
+    directories.Add(path);
+    foreach (var dir in Directory.GetDirectories(path)) TraverseDirectory(dir, directories);
+}
+
+static void PrintDirectoriesAndFiles(List<string> directories)
+{
+    foreach (var dir in directories)
+    {
+        Console.WriteLine("[DIR] " + dir);
+        foreach (var file in Directory.GetFiles(dir)) Console.WriteLine("  - " + Path.GetFileName(file));
+    }
+}
+
